55de435ddc493094d7bf630a9f335f0bc6f8de84
Ziba Khan <your-github-email@example.com>
2026-06-18 15:58:20 +0530

    deployment changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+// import cors from "cors";
+
+// app.use(cors());
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -7,10 +11,20 @@ import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 9000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(9000, () => console.log("Server running on port 9000"));
+// //  make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(9000, () => console.log("Server running on port " + PORT));
