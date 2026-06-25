bcb6c5daf5e2f17a9c664c355e8f5b79d78f09e6
Ziba Khan <your-github-email@example.com>
2026-06-25 22:29:46 +0530

    Add missing dependencies

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:9000/api' : '/api',
-    withCredentials: true,
-})
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:9000/api"
+      : import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
